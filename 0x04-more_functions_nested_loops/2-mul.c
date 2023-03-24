b864c58836605f292eddff211a4dfcb21624e0d3
alx-zero_day <burangyork@gmail.com>
2023-03-23 20:59:46 -0700

    so excited, am getting the gently

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int mul - multiply two variables(a and b)
+ * @a: first value
+ * @b: first value
+ * Return: mul. result
+ */
+
+int mul(int a, int b)
+
+{
+
+	return(a * b);
+}
