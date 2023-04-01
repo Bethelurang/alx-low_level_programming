38604b2f2980f7d08818e471bbae778c1c9cb860
alx-zero_day <burangyork@gmail.com>
2023-04-01 12:30:02 -0700

    doing hard things is quite interesting

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,24 +3,27 @@
 #include <stdio.h>
 
 /**
- *main -print a random of n
- *
+ * main - print random num
  * Return: 0
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		if 
-			(n > 0)
-	printf("%d is positive\n", n);
-	else if 
-			(n < 0)
-	printf("%d is negative\n", n);
-		else
-		       	printf("%d is zero\n", n);
-
+	if (n > 0)
+	{
+		putchar("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		putchar("%d is zero\n", n);
+	}
+	else
+	{
+		putchar("%d is negative\n", n);
+	}
 	return (0);
 }
